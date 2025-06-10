fdf6789ef6f2fbec1ee2feb57312257fde515352
C0C24018 <c0c2401810@edu.teu.ac.jp>
2025-06-10 16:53:25 +0900

    課題２：押下キーコウカトン移動

--- C0C24018_kadai1.py
+++ C0C24018_kadai1.py
@@ -20,18 +20,27 @@ def main():
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-        key_lst = pg.key.get_pressed()
 
         kk_rct.move_ip(-1, 0)
+
+        key_lst = pg.key.get_pressed()
+        x = 0
+        y = 0
+
         if key_lst[pg.K_UP]:
-            kk_rct.move_ip(0, -1)
-        if key_lst[pg.K_DOWN]:
-            kk_rct.move_ip(0, +1)
-        if key_lst[pg.K_RIGHT]:
-            kk_rct.move_ip(+2, 0)
-        if key_lst[pg.K_LEFT]:
-            kk_rct.move_ip(-1, 0)
-    
+            x = 0
+            y = -1
+        elif key_lst[pg.K_DOWN]:
+            x = 0
+            y = +1
+        elif key_lst[pg.K_RIGHT]:
+            x = +2
+            y = 0
+        elif key_lst[pg.K_LEFT]:
+            x = -1
+            y = 0
+        kk_rct.move_ip(x, y)
+
         screen.blit(bg_img, [-tmr, 0])
         screen.blit(bg_img2, [-tmr+1600, 0]) #2
         screen.blit(bg_img, [-tmr+3200, 0])#3
